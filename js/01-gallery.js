4955eb9a41b8fd9601f34c4586897597119d66ab
Serhii Rybak <serhiirybak@Serhiis-MacBook-Pro.local>
2023-12-19 22:40:34 +0100

    Added image gallery code

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,74 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+
+renderGalleryItems(galleryItems);
+
+galleryContainer.addEventListener("click", handleImageClick);
+
+/**
+ * Opens modal window with original image
+ * @param {*} e
+ * @returns
+ */
+function handleImageClick(e) {
+  e.preventDefault();
+
+  const image = e.target;
+
+  if (!image.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+        <img src="${image.dataset.original}" width="800" height="600">
+    `);
+
+  instance.show();
+
+  initLightBoxActions(instance);
+}
+
+/**
+ * Initializes lightbox actions
+ * @param {BasicLightBox} instance
+ */
+function initLightBoxActions(instance) {
+  window.addEventListener("keydown", onEscKeyPress);
+
+  function onEscKeyPress(event) {
+    if (event.code === "Escape") {
+      instance.close();
+      window.removeEventListener("keydown", onEscKeyPress);
+    }
+  }
+}
+
+/**
+ * Renders gallery items
+ * @param galleryItems
+ */
+function renderGalleryItems(galleryItems) {
+  galleryContainer.innerHTML = galleryItems.map(getImageHtml).join("");
+}
+
+/**
+ * Returns gallery item html
+ * @param galleryItem
+ * @returns
+ */
+function getImageHtml({ preview, original, description }) {
+  return `<li class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-original="${original}"
+                    alt="${description}"
+                    />
+                </a>
+            </li>`;
+}
